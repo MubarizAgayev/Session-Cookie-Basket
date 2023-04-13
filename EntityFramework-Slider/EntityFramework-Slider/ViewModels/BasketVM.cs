cf0056cd5be20e22909ea51170ceaea517b8877d
mubarizra@code.edu.az <mubarizra@code.edu.az>
2023-04-13 07:58:32 +0400

    Task

---
+++ EntityFramework-Slider/EntityFramework-Slider/ViewModels/BasketVM.cs
@@ -0,0 +1,14 @@
+﻿namespace EntityFramework_Slider.ViewModels
+{
+    public class BasketVM
+    {
+
+        public int Id { get; set; }
+        public int Count { get; set; }
+        public int Price { get; set; }
+        public string Image { get; set; }
+        public int TotalPrice { get; set; }
+
+    }
+
+}
